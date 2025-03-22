551a0e175b6bb8c94aa7d521fec31d3b89ade9e2
Tom Dörr <tomdoerr96@gmail.com>
2025-03-22 12:58:59 +0100

    feat: Disable LM caching in dspy_agent configuration

--- dspy_agent/config.py
+++ dspy_agent/config.py
@@ -2,5 +2,5 @@ import dspy
 
 def configure_lm(model_name: str = "deepseek/deepseek-chat") -> None:
     """Centralized language model configuration"""
-    lm = dspy.LM(model_name)
+    lm = dspy.LM(model_name, cache=False)
     dspy.settings.configure(lm=lm)
